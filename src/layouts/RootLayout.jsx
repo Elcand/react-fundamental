905cb340cbe92e8a8ae53b8f3d982de640780d6f
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-07 11:56:45 +0700

    dynamic style for navlink

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,30 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import "../styles/index.css";
 
 function RootLayout() {
   return (
     <>
-      <Link to="/">Home</Link> | <Link to="/blog">Blog</Link> | <Link to="/about">About</Link>
+      <NavLink to="/" className={({ isActive, isPending}) =>
+        isActive ? "active" : isPending ? "pending" : ""
+        }
+        style={{ 
+          margin: "15px"
+        }}
+        >Home</NavLink> | 
+
+      <NavLink  to="/blog" className={({ isActive, isPending}) =>
+        isActive ? "active" : isPending ? "pending" : ""
+        }
+        style={{ 
+          margin: "15px"
+        }}>Blog</NavLink> |
+
+      <NavLink to="/about" className={({ isActive, isPending}) =>
+        isActive ? "active" : isPending ? "pending" : ""
+        }
+        style={{ 
+          margin: "15px"
+        }}>About</NavLink>
       <Outlet />
     </>
   );
